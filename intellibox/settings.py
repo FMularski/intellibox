3ee9b31c80d4064a0f0ed5459a86b3446280dc8f
Filip Mularski <mularskif@gmail.com>
2021-09-27 12:59:28 +0200

    setup environ

--- intellibox/settings.py
+++ intellibox/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5r(9bpjze1d*&k$6xkm1fl33qp&3om+wqz065^(*(b&sr%!0='
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
