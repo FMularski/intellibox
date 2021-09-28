c1b5cf10a1a8440c842611ad5143e4a3222e39d3
Filip Mularski <mularskif@gmail.com>
2021-09-28 14:39:24 +0200

    added pin field to User model

--- intellibox_auth/models.py
+++ intellibox_auth/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
 
 class User(AbstractUser):
-    pass
+    pin = models.CharField(max_length=4, validators=[MinLengthValidator(4)])
