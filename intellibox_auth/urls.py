e150b3135c49380e8a0f55901bc4941de0a5ce5e
Filip Mularski <mularskif@gmail.com>
2021-09-30 00:33:27 +0200

    implemented password reset

--- intellibox_auth/urls.py
+++ intellibox_auth/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -7,4 +8,21 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('test-login/', views.TestLoginView.as_view(), name='test_login'),
+
+    path('password_reset/', 
+        auth_views.PasswordResetView.as_view(template_name='intellibox_auth/password_reset.html'), 
+        name='password_reset'
+    ),
+    path('password_reset/done/', 
+        auth_views.PasswordResetDoneView.as_view(template_name='intellibox_auth/password_reset_done.html'), 
+        name='password_reset_done'
+    ),
+    path('password_reset/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='intellibox_auth/password_reset_confirm.html'), 
+        name='password_reset_confirm'
+    ),
+    path('password_reset/complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name='intellibox_auth/password_reset_complete.html'), 
+        name='password_reset_complete'
+    ),
 ]
